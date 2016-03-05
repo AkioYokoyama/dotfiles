20bfa7f9ac8f3985aac61eb055bec0b01c7f53da
vagrant <3924nxoz@jcom.home.ne.jp>
2016-03-05 05:59:15 +0000

    シェル修正

--- symbolic_link.sh
+++ symbolic_link.sh
@@ -1,3 +1,5 @@
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
