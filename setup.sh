d593d8232b120e3ae6f83a4ce653398282d953a9
Yoika <ama_harumaki@yahoo.co.jp>
2020-08-15 16:34:04 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# symbolic link
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.ctags ~/.ctags
 
+# make file
 touch ~/.local_aliases
+cp ~/dotfiles/.gitconfig ~/
