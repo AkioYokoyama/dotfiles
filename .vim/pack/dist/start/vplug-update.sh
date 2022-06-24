31646088dd5f843f9b6cfe739f51ce6dbea27f54
mk3250d07295y@aol.com <Yoika>
2022-06-24 12:05:29 +0900

    add vimのプラグイン設定のシェル作成

---
+++ .vim/pack/dist/start/vplug-update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd ale
+git pull origin master
+
+cd ../any-jump.vim
+git pull origin master
+
+cd ../blamer.nvim
+git pull origin master
+
+cd ../indentLine/
+git pull origin master
+
+cd ../vim-dadbod/
+git pull origin master
+
+cd ../vim-dadbod-ui/
+git pull origin master
+
+cd ../vim-easy-align/
+git pull origin master
+
+cd ../vim-easy-align/
+git pull origin master
+
+cd ../vim-illuminate/
+git pull origin master
+
+cd ../vim-quickrun/
+git pull origin master
+
+cd ../vim-sonictemplate/
+git pull origin master
+
+cd ../vim-surround/
+git pull origin master
