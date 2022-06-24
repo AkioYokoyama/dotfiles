ca5d2ea1f0f6dfb3dc53f669131b768b2cbeb29c
mk3250d07295y@aol.com <Yoika>
2022-06-24 14:37:08 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ bash $HOME/dotfiles/setup/git.sh
 ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -sf $HOME/dotfiles/.vim $HOME/.vim
 ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -sf $HOME/dotfiles/nvim $HOME/.config/nvim
 
 # make file
 touch $HOME/.local_aliases
