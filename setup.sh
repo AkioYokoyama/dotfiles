0b23f70a949a30ae16ac290e69592b5b95ab7b99
akio.yokoyama.yoika@gmail.com <Yoika>
2023-01-15 16:27:51 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@
 bash $HOME/dotfiles/setup/git.sh
 
 # symbolic link
-ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -sf $HOME/dotfiles/.vim $HOME/.vim
-ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -sf $HOME/dotfiles/zshrc $HOME/.zshrc
+ln -sf $HOME/dotfiles/vim $HOME/.vim
+ln -sf $HOME/dotfiles/vimrc $HOME/.vimrc
 ln -sf $HOME/dotfiles/nvim $HOME/.config/nvim
 
 # vim setup
